9bc9d482ececac2de45073e6ca370a8668538345
Rishikumarbaskar <rishibaskar2001@gmail.com>
2025-09-02 23:18:50 +0530

    Cart Update
    Cart Update

--- frontend/src/components/cart/Cart.js
+++ frontend/src/components/cart/Cart.js
@@ -9,29 +9,34 @@ export default function Cart() {
 
 
 return (
+
     <Fragment>
+        {items.length==0?
+        <h2 className="mt-5">Your Cart is Empty</h2>:
+ <Fragment>
       <h2 className="mt-5">Your Cart: <b>{items.length}</b></h2>
       <div className="row d-flex justify-content-between">
         <div className="col-12 col-lg-8">
           <Fragment>
-            <hr />
-            <div className="cart-item">
+            {items.map(item =>(
+                <Fragment>
+                    <div className="cart-item">
               <div className="row">
                 <div className="col-4 col-lg-3">
                   <img
-                    src="https://via.placeholder.com/115"
-                    alt="Product 1"
+                    src={item.image}
+                    alt={item.name}
                     height="90"
                     width="115"
                   />
                 </div>
 
                 <div className="col-5 col-lg-3">
-                  <Link to="#">Sample Product 1</Link>
+                  <Link to={`/product/${item.product}`}>{item.name}</Link>
                 </div>
 
                 <div className="col-4 col-lg-2 mt-4 mt-lg-0">
-                  <p id="card_item_price">$50</p>
+                  <p id="card_item_price">{item.price}</p>
                 </div>
 
                 <div className="col-4 col-lg-3 mt-4 mt-lg-0">
@@ -52,50 +57,15 @@ return (
                 </div>
               </div>
             </div>
-          </Fragment>
-
-          <Fragment>
+            </Fragment>
+            ) )}
             <hr />
-            <div className="cart-item">
-              <div className="row">
-                <div className="col-4 col-lg-3">
-                  <img
-                    src="https://via.placeholder.com/115"
-                    alt="Product 2"
-                    height="90"
-                    width="115"
-                  />
-                </div>
-
-                <div className="col-5 col-lg-3">
-                  <Link to="#">Sample Product 2</Link>
-                </div>
-
-                <div className="col-4 col-lg-2 mt-4 mt-lg-0">
-                  <p id="card_item_price">$30</p>
-                </div>
-
-                <div className="col-4 col-lg-3 mt-4 mt-lg-0">
-                  <div className="stockCounter d-inline">
-                    <span className="btn btn-danger minus">-</span>
-                    <input
-                      type="number"
-                      className="form-control count d-inline"
-                      value="2"
-                      readOnly
-                    />
-                    <span className="btn btn-primary plus">+</span>
-                  </div>
-                </div>
-
-                <div className="col-4 col-lg-1 mt-4 mt-lg-0">
-                  <i id="delete_cart_item" className="fa fa-trash btn btn-danger"></i>
-                </div>
-              </div>
-            </div>
+            
           </Fragment>
 
-          <hr />
+          
+
+          
         </div>
 
         <div className="col-12 col-lg-3 my-4">
@@ -120,6 +90,8 @@ return (
           </div>
         </div>
       </div>
+    </Fragment>
+    }
     </Fragment>
   );
 }
