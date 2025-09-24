2abc7558406f5bc34eddcba19346e97c18f362dc
Rishikumarbaskar <rishibaskar2001@gmail.com>
2025-09-24 22:41:21 +0530

    photos upload for products
    photos upload for products

--- frontend/src/components/layouts/Header.js
+++ frontend/src/components/layouts/Header.js
@@ -2,8 +2,7 @@ import { useEffect, useState } from 'react';
 import { useNavigate, useLocation, Link } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import { logoutUser } from '../../actions/userActions';
-import { Dropdown, Image } from 'react-bootstrap'
-import DropdownToggle from 'react-bootstrap/esm/DropdownToggle';
+import { Dropdown, Image } from 'react-bootstrap';
 
 // Search Component
 function Search() {
@@ -58,76 +57,61 @@ export default function Header() {
     };
 
     return (
-        <nav className="navbar navbar-expand-lg navbar-dark">
-            <div className="container-fluid px-3">
-                <div className="navbar-brand p-0">
-                    <Link to="/" className="d-flex align-items-center text-decoration-none">
-                        <img width="70px" height="70px" src="/images/logo.svg" alt="Shoppers Stop Logo" className="me-2" />
+        <nav className="navbar row">
+            {/* Logo */}
+            <div className="col-12 col-md-3">
+                <div className="navbar-brand">
+                    <Link to="/">
+                        <img width="150px" height="auto" src="/images/logo.svg" alt="Shoppers Stop Logo" />
                     </Link>
                 </div>
+            </div>
 
-                <div className="d-flex d-lg-none align-items-center ms-auto">
-                    <Link to="/cart" className="text-decoration-none me-3">
-                        <span id="cart" className="text-white">Cart</span>
-                        <span className="ms-1" id="cart_count">{cartItems.length}</span>
-                    </Link>
-                    {isAuthenticated ? (
-                        <Dropdown className='d-inline'>
-                            <DropdownToggle variant='default text-white border-0 p-0' id='dropdown-mobile'>
-                                <figure className='avatar avatar-nav'>
-                                    <Image width="35px" height="35px" src={user.avatar ?? './images/default_avatar.png'} className="rounded-circle" />
-                                </figure>
-                            </DropdownToggle>
-                            <Dropdown.Menu align="end">
-                                <div className="px-3 py-2 border-bottom d-lg-none">
-                                    <span className="text-dark fw-bold">Hi, {user?.name}</span>
-                                </div>
-                                {user.role === 'admin' && <Dropdown.Item className='text-dark' onClick={() => { navigate('/admin/dashboard') }}>Dashboard</Dropdown.Item>}
-                                <Dropdown.Item className='text-dark' onClick={() => { navigate('/myprofile') }}>Profile</Dropdown.Item>
-                                <Dropdown.Item className='text-dark' onClick={() => { navigate('/orders') }}>Orders</Dropdown.Item>
-                                <Dropdown.Item className='text-danger' onClick={handleLogout}>Logout</Dropdown.Item>
-                            </Dropdown.Menu>
-                        </Dropdown>
-                    ) : (
-                        <div className="btn btn-sm" id="login_btn" onClick={() => navigate('/login')}>
-                            Login
-                        </div>
-                    )}
-                </div>
+            {/* Search */}
+            <div className="col-12 col-md-6 mt-2 mt-md-0">
+                <Search />
+            </div>
 
-                <div className="collapse navbar-collapse d-none d-lg-block" id="navbarNav">
-                    <div className="d-flex justify-content-center flex-grow-1">
-                        <Search />
-                    </div>
+            {/* Right Side */}
+            <div className="col-12 col-md-3 mt-4 mt-md-0 text-center">
+                {isAuthenticated ? (
+                    <Dropdown className="d-inline">
+                        <Dropdown.Toggle variant="default text-white pr-5" id="dropdown-basic">
+                            <figure className="avatar avatar-nav">
+                                <Image
+                                    width="50px"
+                                    height="50px"
+                                    src={user.avatar ?? './images/default_avatar.png'}
+                                    className="rounded-circle"
+                                />
+                            </figure>
+                            <span>{user?.name}</span>
+                        </Dropdown.Toggle>
+                        <Dropdown.Menu>
+                            {user.role === 'admin' && (
+                                <Dropdown.Item className="text-dark" onClick={() => { navigate('/admin/dashboard') }}>
+                                    Dashboard
+                                </Dropdown.Item>
+                            )}
+                            <Dropdown.Item className="text-dark" onClick={() => { navigate('/myprofile') }}>
+                                Profile
+                            </Dropdown.Item>
+                            <Dropdown.Item className="text-dark" onClick={() => { navigate('/orders') }}>
+                                Orders
+                            </Dropdown.Item>
+                            <Dropdown.Item className="text-danger" onClick={handleLogout}>
+                                Logout
+                            </Dropdown.Item>
+                        </Dropdown.Menu>
+                    </Dropdown>
+                ) : (
+                    <Link to="/login" className="btn" id="login_btn">Login</Link>
+                )}
 
-                    <div className="d-flex align-items-center">
-                        <Link to="/cart" className="text-decoration-none me-5">
-                            <span id="cart" className="text-white">Cart</span>
-                            <span className="ms-1" id="cart_count">{cartItems.length}</span>
-                        </Link>
-
-                        {isAuthenticated ? (
-                            <Dropdown className='d-inline'>
-                                <DropdownToggle variant='default text-white border-0 p-0' id='dropdown-basic'>
-                                    <figure className='avatar avatar-nav me-3'>
-                                        <Image width="35px" height="35px" src={user.avatar ?? './images/default_avatar.png'} className="rounded-circle" />
-                                    </figure>
-                                    <span className="ms-2">Hi, {user?.name}</span>
-                                </DropdownToggle>
-                                <Dropdown.Menu align="end">
-                                    {user.role === 'admin' && <Dropdown.Item className='text-dark' onClick={() => { navigate('/admin/dashboard') }}>Dashboard</Dropdown.Item>}
-                                    <Dropdown.Item className='text-dark' onClick={() => { navigate('/myprofile') }}>Profile</Dropdown.Item>
-                                    <Dropdown.Item className='text-dark' onClick={() => { navigate('/orders') }}>Orders</Dropdown.Item>
-                                    <Dropdown.Item className='text-danger' onClick={handleLogout}>Logout</Dropdown.Item>
-                                </Dropdown.Menu>
-                            </Dropdown>
-                        ) : (
-                            <div className="btn" id="login_btn" onClick={() => navigate('/login')}>
-                                Login
-                            </div>
-                        )}
-                    </div>
-                </div>
+                <Link to="/cart" className="ml-3">
+                    <span id="cart">Cart</span>
+                </Link>
+                <span className="ml-1" id="cart_count">{cartItems.length}</span>
             </div>
         </nav>
     )
