88b0bb011cb70aa46b570c4b7d5f3565ea20d575
Rishikumarbaskar <rishibaskar2001@gmail.com>
2025-09-27 16:21:23 +0530

    email functionality
    email functionality

--- backend/utils/email.js
+++ backend/utils/email.js
@@ -6,7 +6,8 @@ const sendEmail = async options => {
     const transport = {
         host: process.env.SMTP_HOST,
         port: process.env.SMTP_PORT,
-        secure: true,
+        secure: false,
+        requireTLS: true,
         auth: {
             user: process.env.SMTP_USER,
             pass: process.env.SMTP_PASS
